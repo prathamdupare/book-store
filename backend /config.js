6e77e1b95fcd2c1947239ce8799648cb45d700f5
prathamdupare <prathamdupare@proton.me>
2024-01-19 17:00:57 +0530

    added mongoose

---
+++ backend /config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://prathmeshdupare:DXDHOwy3V6EoBbLH@book-store.k3fsnte.mongodb.net/?retryWrites=true&w=majority";
---
+++ backend /index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log(req);
+  return res.status(234).send("Welcome to MERN STACK");
+});
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App is connected to database");
+    app.listen(PORT, () => {
+      console.log(`Listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
